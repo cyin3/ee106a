5864063bedddca352b76b953d1ae12495e9187f5
cyin3 <33710032+cyin3@users.noreply.github.com>
2017-12-08 13:34:08 -0800

    Add files via upload

---
+++ project_follower/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/turtlebot/project_follower/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/home/turtlebot/project_follower/devel/lib:/opt/ros/kinetic/lib"
+export PKG_CONFIG_PATH="/home/turtlebot/project_follower/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig"
+export PWD="/home/turtlebot/project_follower/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/turtlebot/project_follower/devel/share/common-lisp"
